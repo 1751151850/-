0f16f63cb5502a70c2dd8ea3b5ef0ef672f2fee6
caijiaming <1751151850@qq.com>
2020-10-16 17:16:14 +0800

    hsaodoa

---
+++ aaa/aaa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aaa
+//
+//  Created by 蔡佳明 on 2020/10/16.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
